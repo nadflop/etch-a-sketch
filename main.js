7ab17fc764b728b1fcff3037e857f388d0b9bd83
nad <31370689+nadflop@users.noreply.github.com>
2024-05-15 16:09:44 +0900

    Completed etch-a-sketch project. - changed background color to be random rgb instead of purple - created a function to regenrate grid and clear grid after getting user's input - styled the webpage to center the sketchpad and button

--- main.js
+++ main.js
@@ -1,13 +1,61 @@
-const container = document.querySelector("#container");
+function randomInt(max) {
+    return Math.floor(Math.random()*(max + 1));
+}
 
-function createGrid() {
-    for (let i = 0; i < 16; i ++) {
-        for (let j = 0; j < 16; j++) {
+function randomRgbColor() {
+    var r = randomInt(255);
+    var g = randomInt(255);
+    var b = randomInt(255);
+    return `rgb(${r},${g},${b})`;
+}
+
+function addHover() {
+    const div = document.querySelectorAll(".grid");
+    div.forEach(element => {
+        element.addEventListener("mouseenter", (event) => {
+        element.style.backgroundColor = randomRgbColor();
+        })
+    });
+}
+
+const container = document.querySelector("#container");
+function createGrid(gridNum) {
+    for (let i = 0; i < gridNum; i ++) {
+        for (let j = 0; j < gridNum; j++) {
             const newDiv = document.createElement("div");
             newDiv.className = "grid";
+            newDiv.style.width = (100/gridNum) + "vh";
+            newDiv.style.height = (100/gridNum) + "vh";
             container.append(newDiv);
         }
     }
+    addHover();
 }
 
-createGrid();
+function clearGrid() {
+    const grid = document.querySelectorAll(".grid");
+    grid.forEach(element => {
+        element.remove();
+    });
+}
+
+function regenerateGrid(gridNum) {
+    clearGrid();
+    createGrid(gridNum);
+}
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    var gridNum = prompt(`How many number of squares per side for the new grid?
+    Pick between 1 and 100.`);
+    if (!(/\s/).test(gridNum)) gridNum = parseInt(gridNum);
+    while (gridNum <= 0 || !Number.isInteger(gridNum) || gridNum > 100) {
+        gridNum = prompt(`How many number of squares per side for the new grid?
+        Pick between 1 and 100.`);
+        //check if the string contains whitespace char since we only want 1 input
+        if (!(/\s/).test(gridNum)) gridNum = parseInt(gridNum);
+    }
+    regenerateGrid(gridNum);
+});
+
+window.onload = createGrid(16); //creates 16x16 grid by default
